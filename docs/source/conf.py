ab5382d600abbe839883c7a93ab2cbcf4f1d57ff
19ecal <96256923+mrlegohead0x45@users.noreply.github.com>
2022-01-15 18:30:48 +0000

    changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(r'..\..\beans'))
+sys.path.insert(0, os.path.abspath(r'../../beans'))
 
 
 # -- Project information -----------------------------------------------------
