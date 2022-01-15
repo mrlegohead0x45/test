1dc2f95255045a4a9d67802c865c65f52bf531ff
19ecal <96256923+mrlegohead0x45@users.noreply.github.com>
2022-01-15 18:20:26 +0000

    changes

---
+++ beans/beans.py
@@ -0,0 +1,20 @@
+"""beans are great"""
+
+def beansfunc():
+	"""beans is great"""
+	pass
+
+class Beans:
+	"""class to represent some beans"""
+	def __init__(self, beans: int) -> None:
+		pass
+
+	def getbeans() -> int:
+		"""method to get the beans"""
+		pass
+
+beans: int
+"""number of beans to make"""
+
+
+	
---
+++ docs/build/html/_sources/beans.rst.txt
@@ -0,0 +1,5 @@
+beans
+-----
+
+.. automodule :: beans
+   :members:
